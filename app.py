960bf5f6080af561cb542c831827f267cc3d47c0
jayakrishnagaddam <115868714+jayakrishnagaddam@users.noreply.github.com>
2023-10-12 15:02:31 +0000

    Added Jobs and Apply Button for the careers page

--- app.py
+++ app.py
@@ -3,9 +3,36 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+JOBS=[{
+  'id':'1',
+  'title':"Data Analyst",
+  'Location':"Hyderabad, India",
+  'Salary':"1000000"
+  
+},
+     {
+  'id':'2',
+  'title':"Data Scientist",
+  'Location':"Hyderabad, India",
+  'Salary':"2000000"
+  
+},{
+  'id':'3',
+  'title':"Frontend engineer",
+  'Location':"Hyderabad, India",
+  'Salary':"3000000"
+  
+},
+     {
+  'id':'4',
+  'title':"Backend Engineer",
+  'Location':"Hyderabad, India",
+  'Salary':"3000000"
+}]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS)
 
 
 if __name__ == '__main__':
